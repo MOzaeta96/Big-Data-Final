a769d44e5eeb50de69c727ca36ad70049c49f7f6
Marty <42067325+MOzaeta96@users.noreply.github.com>
2024-12-10 14:12:04 -0500

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,5 @@
-plugins {
-    kotlin("jvm") version "1.9.22"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22" apply false
-}
+dependencies {
+    implementation(project(":support:workflow-support"))
 
-subprojects {
-    if (listOf("applications", "components", "support").contains(name)) return@subprojects
-
-    apply(plugin = "kotlin")
-
-    repositories {
-        mavenCentral()
-    }
-
-    dependencies {
-        implementation("org.slf4j:slf4j-api:2.0.7")
-
-        testImplementation(kotlin("test-junit"))
-        implementation("org.jetbrains.exposed:exposed-core:0.41.1")
-        implementation("org.jetbrains.exposed:exposed-dao:0.41.1")
-        implementation("org.jetbrains.exposed:exposed-jdbc:0.41.1")
-        implementation("org.postgresql:postgresql:42.5.0")
-        implementation("io.micrometer:micrometer-registry-prometheus:1.10.2")
-
-    }
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 }
